4e445b4cdd0a0f27c9b29740b5dba31716f17b5e
alimukhammad <47307714+alimukhammad@users.noreply.github.com>
2024-10-31 00:01:38 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ let express = require('express');
 let app = express();
 
 
-
+// app.Method(Path, Handler, Middleware)
+// function(req obj, res obj) || function(req, res next)
 console.log("Hello World");
 
 
